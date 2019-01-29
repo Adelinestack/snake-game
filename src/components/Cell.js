705b53f6eaa985d16e8c79eab12b72ada8286e5a
Adelinestack <adeline.colombo@stackerine.com>
2019-01-29 16:59:45 +0100

    snake with n position move

---
+++ src/components/Cell.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Cell(props) {
+  return <div className={props.value} />;
+}
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Cell from './Cell';
 import './grid.css';
 
 export default class Grid extends Component {
@@ -8,7 +8,8 @@ export default class Grid extends Component {
     this.grid = null;
     this.state = {
       direction: 'right',
-      snakePosition: [0, 0],
+      snakePosition: [[0, 0], [0, 1], [0, 2]],
+      foodPosition: [0, 0],
     };
   }
 
@@ -20,7 +21,7 @@ export default class Grid extends Component {
   gameStart() {
     setInterval(() => {
       this.moveSnake(this.state.direction);
-    }, 300);
+    }, 1000);
   }
 
   onKeyPress({ key }) {
@@ -36,53 +37,59 @@ export default class Grid extends Component {
   }
 
   moveSnake(direction) {
+    const [snakeQueue, ...snake] = this.state.snakePosition;
+
     if (direction === 'right') {
       this.setState({
         snakePosition: [
-          this.state.snakePosition[0],
-          this.state.snakePosition[1] + 1,
+          ...snake,
+          [snake[snake.length - 1][0], snake[snake.length - 1][1] + 1],
         ],
       });
     } else if (direction === 'left') {
       this.setState({
         snakePosition: [
-          this.state.snakePosition[0],
-          this.state.snakePosition[1] - 1,
+          ...snake,
+          [snake[snake.length - 1][0], snake[snake.length - 1][1] - 1],
         ],
       });
     } else if (direction === 'up') {
       this.setState({
         snakePosition: [
-          this.state.snakePosition[0] - 1,
-          this.state.snakePosition[1],
+          ...snake,
+          [snake[snake.length - 1][0] - 1, snake[snake.length - 1][1]],
         ],
       });
     } else if (direction === 'down') {
       this.setState({
         snakePosition: [
-          this.state.snakePosition[0] + 1,
-          this.state.snakePosition[1],
+          ...snake,
+          [snake[snake.length - 1][0] + 1, snake[snake.length - 1][1]],
         ],
       });
     }
   }
 
   isCellHasSnake(rowIndex, colIndex) {
-    if (
-      this.state.snakePosition[0] === rowIndex &&
-      this.state.snakePosition[1] === colIndex
-    ) {
-      return <div className="snake" />;
+    const isSnake = this.state.snakePosition.find(
+      position => position[1] === colIndex && position[0] === rowIndex
+    );
+    if (isSnake) {
+      return 'snake';
     }
-    return <div className="cell" />;
+    return 'cell';
   }
 
   render() {
     const { grid } = this.props;
     const rows = grid.map((row, rowIndex) => {
-      const cols = row.map((col, colIndex) =>
-        this.isCellHasSnake(rowIndex, colIndex)
-      );
+      const cols = row.map((col, colIndex) => (
+        <Cell
+          value={this.isCellHasSnake(rowIndex, colIndex)}
+          rowIndex={rowIndex}
+          colIndex={colIndex}
+        />
+      ));
       return <div className="grid-line">{cols}</div>;
     });
 
